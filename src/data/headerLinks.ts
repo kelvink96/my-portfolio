c9009ac31ff75e9e254b279d09fd7294cb60011e
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2022-07-26 23:59:25 +0300

    basic structure complete

---
+++ src/data/headerLinks.ts
@@ -0,0 +1,16 @@
+export const links = {
+	"links": [
+		{
+			"link": "/about",
+			"label": "about"
+		},
+		{
+			"link": "/skills",
+			"label": "skills"
+		},
+		{
+			"link": "/projects",
+			"label": "projects"
+		},
+	]
+}
---
+++ src/data/socialLinks.ts
@@ -0,0 +1,43 @@
+import {
+	BrandBehance,
+	BrandDribbble,
+	BrandFacebook,
+	BrandInstagram,
+	BrandLinkedin,
+	BrandYoutube
+} from "tabler-icons-react";
+
+export const socialLinks = {
+	"links": [
+		{
+			"link": "https://www.facebook.com/kelvinkk96",
+			"label": "facebook",
+			"icon": BrandFacebook
+		},
+		{
+			"link": "https://www.linkedin.com/in/kelvink96/",
+			"label": "linkedin",
+			"icon": BrandLinkedin
+		},
+		{
+			"link": "https://www.instagram.com/kelvink_96/",
+			"label": "instagram",
+			"icon": BrandInstagram
+		},
+		{
+			"link": "https://www.youtube.com/channel/UCtSLVZBTdEw475v3NmKfQ3g",
+			"label": "youtube",
+			"icon": BrandYoutube
+		},
+		{
+			"link": "https://dribbble.com/kelvink96",
+			"label": "dribbble",
+			"icon": BrandDribbble
+		},
+		{
+			"link": "https://www.behance.net/kelvink96",
+			"label": "behance",
+			"icon": BrandBehance
+		},
+	]
+}
