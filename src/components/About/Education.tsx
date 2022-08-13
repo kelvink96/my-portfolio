51600b68440d88b0e86646c36c474de881695af3
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2022-08-13 16:32:26 +0300

    - updated about me section

--- src/components/About/Education.tsx
+++ src/components/About/Education.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import {Card, Text, Box, Title} from "@mantine/core";
-import {School} from "tabler-icons-react";
+import {Card, Text, Box, Title, createStyles} from "@mantine/core";
+
+const useStyles = createStyles(() => ({
+	content: {
+		textAlign: "left"
+	},
+	title: {
+		textAlign: "left"
+	}
+}))
 
 const Education = () => {
+	const {classes} = useStyles()
 	return (
 		<div>
 			<Box>
 				<Title align="left">Education</Title>
 			</Box>
-			<Card p="md" withBorder>
+			<Card p="md" withBorder className={classes.content}>
 				<Text weight={500} mb="sm">Bachelor of Science in Software Engineering</Text>
 				<Text>Kisii University</Text>
 				<Text>2014 - 2018</Text>
